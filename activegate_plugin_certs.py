9133cf214388d277a1303f23d90288ae944ea781
leonvzGit <leonvz@gmail.com>
2020-03-17 06:29:16 +0200

    release 1.014

--- activegate_plugin_certs.py
+++ activegate_plugin_certs.py
@@ -26,18 +26,22 @@ class CertsPluginRemote(RemoteBasePlugin):
         logger.info("Config: %s", config)
         self.path = os.getcwd()
         self.period = config["period"]
+        self.alert_period = config["alert_period"]
+        self.poll_period = config["poll_interval"]
+
         self.hosts = config["hosts"].split(",")
         self.default_expiry_warn = self.period
-        self.alert_interval = self.config.get("alert_interval", 10)
-        self.event_interval = self.config.get("event_interval", 3)
-        self.relative_interval = self.config.get("relative_interval", 60)
-        self.state_interval = self.config.get("state_interval", 60)
+        self.default_expiry_err = self.alert_period
+        
+        self.alert_interval = self.config.get("alert_interval")
+        self.event_interval = self.config.get("event_interval")
+        
+        
 
         self.alert_iterations = 0
         self.event_iterations = 0
-        self.relative_iterations = 0
         self.absolute_iterations = 0
-        self.state_iterations = 0
+        
 
         self.current_entries = 1
         self.archived_entries = 0
@@ -47,28 +51,25 @@ class CertsPluginRemote(RemoteBasePlugin):
 
         self.domains = None
 
-        if (self.hosts is not None):
-            self.domains = list(check_tls_certs.itertools.chain(
-                check_tls_certs.domain_definitions_from_cli(self.hosts)))
-        else:
-            self.domains = list(check_tls_certs.itertools.chain(
-                check_tls_certs.domain_definitions_from_filename(self.path + "/hosts.txt")))
-     
+        
 
-
-        self.domain_certs = check_tls_certs.get_domain_certs(self.domains)
-        self.exceptions = list(x for x in self.domain_certs.values() if isinstance(x, Exception))
-        self.total_warnings = 0
-        self.total_errors = 0
-        self.earliest_expiration = None
-        self.utcnow = datetime.datetime.utcnow()
-        self.checked_domains = check_tls_certs.check_domains(self.domains, self.domain_certs, self.utcnow, expiry_warn=self.default_expiry_warn)
-        print(self.checked_domains)
-       
         
     
 
 
+
+        
+          
+       
+
+    def query(self, **kwargs):
+        # Create group - provide group id used to calculate unique entity id in dynatrace
+        #   and display name for UI presentation
+
+        if self.should_poll_cert():
+            self.poll_hosts()
+        else:
+            logger.info("Hosts not being polled:%s < %s",self.absolute_iterations, self.poll_period)
 
         for domainnames, msgs, expiration in self.checked_domains:
             if expiration:
@@ -109,13 +110,8 @@ class CertsPluginRemote(RemoteBasePlugin):
             click.echo(click.style(msg, fg="red"))
           
         elif self.total_warnings:
-            click.echo(click.style(msg, fg="yellow"))
-          
-       
-
-    def query(self, **kwargs):
-        # Create group - provide group id used to calculate unique entity id in dynatrace
-        #   and display name for UI presentation
+            click.echo(click.style(msg, fg="yellow"))    
+        
         group = self.topology_builder.create_group(identifier="CertGroup",
                                                    group_name="SSL Expiration Group")
 
@@ -131,8 +127,8 @@ class CertsPluginRemote(RemoteBasePlugin):
             return str(abs((d2 - d1).days))  
         
 
-        # Push infrastructure problems
-       
+        # Push infrastructure info events and problems
+        
         for domainnames, msgs, expiration in self.checked_domains:
             if expiration:
                 print("expiration: " + str(expiration))
@@ -144,61 +140,84 @@ class CertsPluginRemote(RemoteBasePlugin):
             print("domainnames: ", domainnames)
             for level, msg in msgs:
                 if level == 'warning' and "The certificate expires" in msg:
-                    logger.info("Logging Problem for Domain:%s, Warning:%s", domainnames,msg)
+                   
                     days = days_between(str(expiration))
                     device = group.create_device(identifier= domainnames[0],
                                                  display_name=domainnames[0])
-                    logger.info("Topology: group name=%s, node name=%s", group.name, device.name)
-                    device.report_error_event(title="Certificate Expiration Within the threshold set: " + str(self.default_expiry_warn) + " days",
+                    if (self.absolute_iterations ==1):
+                        print("Iterations: ", self.absolute_iterations)
+                        if int(days) > int(self.default_expiry_err) and int(days) <= int(self.default_expiry_warn):
+                            logger.info("Logging Info Event for Domain:%s, Warning:%s", domainnames,msg)
+                            device.report_custom_info_event(title="Certificate Expiration Within the Warning threshold set: " + str(self.default_expiry_warn) + " days",
+                                      description="The SSL Cerficate  will expire in: "+ days + " days",
+                                      properties={"exp_date": str(expiration),
+                                                  "exp_days": str(days)
+                                      
+                                      }
+                                      )
+                                                  
+                        else:
+                            logger.info("Logging Problem Alert for Domain:%s, Warning:%s", domainnames,msg)
+                            logger.info("Topology: group name=%s, node name=%s", group.name, device.name)
+                            device.report_error_event(title="Certificate Expiration Within the Alert threshold set: " + str(self.default_expiry_err) + " days",
                                 description="The SSL Cerficate  will expire in: "+ days + " days",
                                 properties={"exp_date": str(expiration),
-
+                                             "exp_days": str(days)
                                 }
                                 
                                 )
-
+                    else:
+                        logger.info("Event not being logged to prevent spam:%s",self.absolute_iterations)
     
+    def poll_hosts(self):
+        if (self.hosts is not None):
+            logger.info("Polling hosts")
+            self.domains = list(check_tls_certs.itertools.chain(
+                check_tls_certs.domain_definitions_from_cli(self.hosts)))
+        else:
+            self.domains = list(check_tls_certs.itertools.chain(
+                check_tls_certs.domain_definitions_from_filename(self.path + "/hosts.txt")))
+     
+        logger.info("Iterations:%s",self.absolute_iterations)
+        
+        self.domain_certs = check_tls_certs.get_domain_certs(self.domains)
+        self.exceptions = list(x for x in self.domain_certs.values() if isinstance(x, Exception))
+        self.total_warnings = 0
+        self.total_errors = 0
+        self.earliest_expiration = None
+        self.utcnow = datetime.datetime.utcnow()
+        self.checked_domains = check_tls_certs.check_domains(self.domains, self.domain_certs, self.utcnow, expiry_warn=self.default_expiry_warn)
+           
+        print("Checked Domains:", self.checked_domains)
+        
 
+        
+    
+    
+    def should_poll_cert(self):
+        self.absolute_iterations = self.absolute_iterations + 1
+        
+        if (self.absolute_iterations > self.poll_period):
+            self.absolute_iterations = 0
+            return True
+
+        if self.absolute_iterations ==1:
+             return True   
+
+        return False   
+    
+    def should_create_event(self):
+        self.event_iterations = self.event_iterations + 1
+        if self.event_iterations > self.event_interval:
+            self.event_iterations = 0
+            return True
+        return False    
+
+    def should_create_error(self):
+        self.alert_iterations = self.alert_iterations + 1
+        if self.alert_iterations > self.alert_interval:
+            self.alert_iterations = 0
+            return True
+        return False    
    
-        # Report problems
-        """
-        device.report_performance_event(title="Performance Event",
-                                      description="Use it to focus on some performance issue",
-                                      properties={"property_key": "property_value"})
-
-        device.report_error_event(title="Error Event",
-                                description="Use it to report some error",
-                                properties={"property_key": "property_value"})
-        logger.info("Report error event")
-
-        device.report_availability_event(title="Availability Event",
-                                       description="Use it to focus on some availability issue",
-                                       properties={"property_key": "property_value"})
-        logger.info("Report availability event")
-
-        device.report_resource_contention_event(title="Resources Contention Event",
-                                              description="Use it to focus on some resource contention issue",
-                                              properties={"property_key": "property_value"})
-        logger.info("Report resource contention event")
-
-        # report information
-        device.report_custom_info_event(title="Custom Info Event",
-                                      description="Use it to report some custom info",
-                                      properties={"property_key": "property_value"})
-        logger.info("Report custom info event")
-
-        device.report_custom_deployment_event(source="demo source",
-                                            project="demo plugin",
-                                            version="1.001",
-                                            ci_link=self.url + "/deployment",
-                                            remediation_action_link=self.url + "/remediation",
-                                            deployment_name="Demo deployment",
-                                            properties={"property_key": "property_value"})
-        logger.info("Report custom deployment event")
-
-        device.report_custom_annotation_event(description="Annotation event",
-                                            annotation_type="demo",
-                                            source="demo source",
-                                            properties={"property_key": "property_value"})
-        logger.info("Report custom annotation event")
-"""
+               
